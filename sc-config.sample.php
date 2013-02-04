3a674f549134850de905c929874995bd9cf8e023
Rick Ootes <rick.ootes@gmail.com>
2013-02-04 20:39:04 +0100

    Adding a config sample for the application remove the .sample suffix to use it

---
+++ sc-config.sample.php
@@ -0,0 +1,12 @@
+<?php 
+
+// Database configuration
+define('HOST', '');
+define('USER', '');
+define('PASS', '');
+define('NAME', '');
+
+// Set the max number of atempts to login
+define('NUMBER_OF_ATTEMPTS', 6);
+
+?>
