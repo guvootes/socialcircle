9173fd8bd18b06f07c620e344d996e8a40f6ff12
Rick Ootes <rick.ootes@gmail.com>
2013-02-05 19:45:09 +0100

    new methods in user class

--- api/classes/user.php
+++ api/classes/user.php
@@ -6,13 +6,36 @@
 
 			// $data: username, email, password, birthday
 
-			if ( strlen($data['username']) < 3 ) {
-				return json_encode(array(
-					'error' => true,
-					'message' => $username . ' seems a bit short. Minimum 3 chars!'
-				));
+			$errors = array();
+
+			// Check for username length
+			if ( strlen($data['username']) <= 3 ) {
+				$errors['username'] = 'Uw gebruikersnaam is te kort, minimaal 3 tekens';
 			}
+
+			// Check for email
+
+			// Check for password length
+			if ( strlen($data['password']) <= 3 ) {
+				$errors['password'] = 'Uw wachtwoord is te kort, minimaal 3 tekens';
+			}
+
+			
+
+
 		}
+
+
+		protected function in_use($key, $value, $table) {
+
+			$stmt = $this->db->prepare('SELECT * from '.$table.' where '.$item.' = ":value"');
+			$stmt->bindParam(':value', $value);
+			$stmt->execute();
+
+			return $stmt->rowCount();
+
+		}
+
 	}
 
 ?>
