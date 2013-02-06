babe0d5b4f5b6fef980be29cf9dfb4875701fbe8
ootes <mark.ootes@gmail.com>
2013-02-06 11:59:51 +0100

    Modified register page

--- js/models/userModel.js
+++ js/models/userModel.js
@@ -9,11 +9,28 @@ define([], function(){
 		validate: function (attrs) {
 	    	var errors = [];
 	  
-	     	if (!attrs.email) {
-	        	errors.push({name: 'email', message: 'Please fill email field.'});
-	     	}
+	  		// username validation
 	     	if (!attrs.username) {
-	        	errors.push({name: 'username', message: 'Please fill username field.'});
+	        	errors.push({name: 'username', message: 'Vul een gebruikersnaam in'});
+	     	}else if(attrs.username.length < 4 ){
+	     		errors.push({name: 'username', message: 'De opgegeven naam is te kort'});
+	     	}
+	     	
+	     	// email validation
+	     	if (!attrs.email) {
+	     		errors.push({name: 'email', message: 'Vul een e-mailadres in'});
+	     	}
+	     	
+	     	// password validation
+	     	if (!attrs.password) {
+	     		errors.push({name: 'password', message: 'Vul een wachtwoord in'});
+	     	}else if(attrs.password.length < 5 ){
+	     		errors.push({name: 'password', message: 'Het wachtwoord moet ten minste 5 tekens bevatten'});
+	     	}
+	     	
+	     	// birthday validation
+	     	if (!attrs.birthday) {
+	     		errors.push({name: 'birthday', message: 'Vul een geboortedatum in'});
 	     	}
 	  
 	     	return errors.length > 0 ? errors : false;
--- js/views/registerView.js
+++ js/views/registerView.js
@@ -21,20 +21,41 @@
 			submitForm: function(e){
 				e.preventDefault();
 				
+				var self = this;
 				var newUser = new userModel();
 				
 				var data = {
-					email: this.$('#email').val(),
-					username:  this.$('#username').val()
-				}
+					username	: this.$('#username').val(),
+					email		: this.$('#email').val(),
+					password	: this.$('#password').val(),
+					birthday	: this.$('#birthday').val()					
+				};
 				 
 				newUser.save(data,{
-						error: function(model, error){ 
-							console.log(error);
-						}
+					error: function(model, error){
+					
+						$(".formlist li").removeClass('error').find('span').remove();
+						 
+						self.showErrors(error);
+					},
+					succes: function(){
+						self.hideErrors();
 					}
-				);		
+				});		
 				
+			},
+			
+			showErrors: function(errors) {
+			    _.each(errors, function (error) {
+			        var controlGroup = this.$('#' + error.name).parent();
+			        controlGroup.addClass('error');
+			        controlGroup.append("<span class='helper'>"+error.message+"</span>");
+			    }, this);
+			},
+			 
+			hideErrors: function () {
+			    this.$('.formlist li').removeClass('error');
+			    this.$('.helper').remove();
 			},
 
 			render: function () {
